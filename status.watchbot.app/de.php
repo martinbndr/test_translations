06c552be9eff0eaee9432a9c82691f74d8a1b30a
Martin <box152535@gmail.com>
2023-02-24 11:16:24 +0100

    Test

---
+++ status.watchbot.app/de.php
@@ -0,0 +1,25 @@
+<?php
+
+// incidents.php //
+$norobot = "Dieser Roboter wird nicht von Watchbot überwacht";
+$d90 = "vor 90 Tagen";
+$dtoday = "Heute";
+$pinci = "vergangene Vorfälle";
+$noinci = "Keine Vorfälle berichtet für heute.";
+$mo = "großer Ausfall";
+$po = "Teilausfall";
+
+// incidentsdetails.php //
+$ifx = "INCIDENT REPORT FOR";
+$res = "erledigt";
+$mon = "Überwachung";
+$inv = "Untersuchung";
+$ide = "identifiziert";
+$upd = "Update";
+$posted = "gepostet";
+
+$certified = "zertifiziert";
+$partner = "Partner";
+$inci_inprog = "Vorfall im Gange";
+
+?>
---
+++ status.watchbot.app/fr.php
@@ -0,0 +1,25 @@
+<?php
+
+// incidents.php //
+$norobot = "Ce robot n'est pas surveillé par Watchbot";
+$d90 = "Il y a 90 jours";
+$dtoday = "Aujourd'hui";
+$pinci = "DERNIERS INCIDENTS";
+$noinci = "Pas d'incidents reportés ce jour.";
+$mo = "Panne Majeure";
+$po = "Panne Partielle";
+
+// incidentsdetails.php //
+$ifx = "RAPPORT D'INCIDENT POUR";
+$res = "Résolu";
+$mon = "En surveillance";
+$inv = "Recherche";
+$ide = "Identifié";
+$upd = "Mise à Jour";
+$posted = "Posté";
+
+$certified = "Certifié";
+$partner = "Partenaire";
+$inci_inprog = "Incident en cours";
+
+?>
